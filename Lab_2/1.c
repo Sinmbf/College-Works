c8dab4e7ffa59b5d2aae3ef15bfd5dcfc8ada3fc
Sinmbf <sinmbf123@gmail.com>
2024-01-10 22:58:26 +0545

    Finished

--- Lab_2/1.c
+++ Lab_2/1.c
@@ -1,5 +1,11 @@
+// WAP to read character from user in lowercase and change it to uppercase
+
 #include<stdio.h>
 
-void main(){
-    printf("Hello World");
+void main() {
+	char ch;
+	printf("Enter a lower case letter: ");
+	scanf("%c",&ch);
+	printf("%c into upper case is %c\n",ch,ch-32);
+
 }

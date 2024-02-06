6aeb9621335c26254d297a4936ca462cdb9f2bea
Sinmbf <sinmbf123@gmail.com>
2024-02-07 02:39:36 +0545

    Add new files

---
+++ Lab_4/19(i).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){
+	int i,j,k;
+	for(i='E';i>='A';i--){
+		for(k='E';k>i;k--){
+			printf(" ");
+		}
+		for(j='A';j<=i;j++){
+			printf("%c ",i);
+		}
+		printf("\n");
+	}
+}

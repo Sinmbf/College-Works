ca0c0d18623625df63d7e685bced998e47e19fd6
Sinmbf <sinmbf123@gmail.com>
2024-02-10 02:15:30 +0545

    Add new file(home)

--- Lab_4/21.c
+++ Lab_4/21.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 
 void main(){
-	
+	int n,i,factorial=1,count=1;
+	printf("Enter a number: ");
+	scanf("%d",&n);
+	if(n>0){
+		goto factorial;
+	}
+	factorial:
+		for(i=1;i<=n;i++){
+			factorial*=i;
+		}
+		printf("The factorial of %d is %d",n,factorial);
 }

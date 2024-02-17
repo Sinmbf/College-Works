1320823dcf1380755a6e0236f28225106a3e666f
Sinmbf <sinmbf123@gmail.com>
2024-02-17 20:17:41 +0545

    Made some changes

--- Lab_5(I)/10.c
+++ Lab_5(I)/10.c
@@ -1,13 +1,19 @@
 #include<stdio.h>
 
 void main(){
-	int n,target,arr[]={5,3,112,1,43,6},i;
+	int n,target=0,arr[]={5,3,112,1,43,6},i,num;
 	n=sizeof(arr)/sizeof(arr[0]); // Find the number of elements in the array
 	printf("Which number do you want to search? ");
-	scanf("%d",&target);
+	scanf("%d",&num);
 	for(i=0;i<n;i++){
-		if(arr[i]==target){
+		if(arr[i]==num){
+			target++;
 			printf("A match found at index %d which is %d",i,arr[i]);
+			break;
 		}
 	}
+	if(target==0){
+		printf("No match found");
+	}
+	
 }
--- Lab_5(I)/13.c
+++ Lab_5(I)/13.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 
 void main(){
-	int count1=0,count2=0,i,n;
+	int count=0,total,i,n;
 	printf("Enter the size of the array: ");
 	scanf("%d",&n);
+	total=n;
 	int a[n];
 	printf("Enter %d values: ");
 	for(i=0;i<n;i++){
 		scanf("%d",&a[i]);
 		if(a[i]%2==0){
-			count1++;
-		}
-		else{
-			count2++;
+			count++;
+			total--;
 		}
 	}
-	printf("Number of even elements: %d\nNumber of odd elements: %d",count1,count2);
+	printf("Number of even elements: %d\nNumber of odd elements: %d",count,total);
 }

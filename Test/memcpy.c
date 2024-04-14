8605660c0236b868add1d854478ecd79b410170d
Sinmbf <sinmbf123@gmail.com>
2024-04-14 23:26:02 +0545

    Update

--- Test/memcpy.c
+++ Test/memcpy.c
@@ -4,8 +4,8 @@
 void main(){
 	char src[50]="Nepal";
 	char dest[50]="Helloooo!!";
-	strcpy(dest,src);
+//	strcpy(dest,src);
 	printf("Before memcpy dest = %s\n",dest);
-	memcpy(dest,src,strlen(src)+1);
+	memcpy(dest,src,5);
 	printf("After memcpy dest = %s\n",dest);
 }

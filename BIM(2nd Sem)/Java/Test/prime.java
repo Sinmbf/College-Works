72e5f6309d9d419a48a5626aa1286298b0db0100
Sinmbf <sinmbf123@gmail.com>
2024-06-17 20:36:07 +0545

    Update

--- BIM(2nd Sem)/Java/Test/prime.java
+++ BIM(2nd Sem)/Java/Test/prime.java
@@ -1,6 +1,6 @@
 class prime{
     public static void main(String[]args){
-        int n=5,flag=1;
+        int n=10,flag=1;
         for(int i=2;i<n;i++){
             if(n%i==0){
                 flag=0;

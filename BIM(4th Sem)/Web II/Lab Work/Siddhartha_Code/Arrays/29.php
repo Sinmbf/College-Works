d9354dc7722eda865690e013443e7ecb053456b2
SinmbfLost <sinmbflost@gmail.com>
2025-07-21 00:57:29 +0545

    Update web lab work upto 32

---
+++ BIM(4th Sem)/Web II/Lab Work/Siddhartha_Code/Arrays/29.php
@@ -0,0 +1,14 @@
+<!-- 29. Create an associative array of countries and their capitals. Display them alphabetically by country name. -->
+
+<?php
+$arr = ["Nepal" => "Kathmandu", "India" => "New Delhi", "United States" => "Washington DC", "China" => "London", "Bangladesh" => "butan"];
+$newArr = [];
+
+$largest = "Nepal";
+
+
+foreach ($arr as $country1 => $capital1) {
+}
+foreach ($newArr as $key => $value)
+    echo $key;
+?>
